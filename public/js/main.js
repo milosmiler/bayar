d0cd275eaa580932f77f4926fde4a89dea9ab489
jairo villalobos <jairo.villalobos@lunave.com>
2019-05-14 18:02:13 -0500

    creacion de actives en menu con vanila js

--- public/js/main.js
+++ public/js/main.js
@@ -13,3 +13,16 @@ close.addEventListener("click", function () {
 	menu.classList.remove('open');
 	overlay.classList.remove('open');
 });
+
+// active al seleccionar menu
+
+let navItem = Array.prototype.slice.apply(document.querySelectorAll('a')); // seleccionamos la etiqueda y la convertimos en un array
+let navItems = document.getElementById('navs'); // cachamos el id de la caja padre para meter el evento click
+navItems.addEventListener('click', e => {
+	if (e.target.classList.contains('item-nav')) {
+		// delegamos el evento  buscando la clase item-nav  para disparar el evento
+		let i = navItem.indexOf(e.target); // cual es la posicion del elemento seleccion solo funciona con los array
+		navItem.map(tab => tab.classList.remove('active'));
+		navItem[i].classList.add('active');
+	}
+});
