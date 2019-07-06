241125fee79f1987f46f4e609f9d2b25f99fc9fe
Jairo villalobos <breakjai@yahoo.com.mx>
2019-07-06 10:03:51 -0500

    ajustes de contacto

--- application/views/site/aviso.php
+++ application/views/site/aviso.php
@@ -10,9 +10,8 @@
       <p>SÍGUENOS <br><span>/ @backyard</span></p>
       <aside class="social"><i class="fab fa-facebook-f"></i><i class="fab fa-twitter"></i><i class="fab fa-instagram"></i></aside>
     </footer>
+    <input type="hidden" value="<?= base_url('aviso-de-privacidad') ?>" id="location">
     <script src="js/jquery-1.11.3.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-scrollTo/2.1.2/jquery.scrollTo.min.js"></script>
-    <script src="//cdn.jsdelivr.net/npm/jquery.localscroll@2.0.0/jquery.localScroll.min.js"></script>
     <script src="public/js/owl.carousel.min.js" type="text/javascript"></script>
     <script src="public/js/main.js" type="text/javascript"></script>
   </body>
